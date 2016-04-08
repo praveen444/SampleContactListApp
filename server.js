b3c4bc655e9683212086d8b119922b0d528a31fb
Praveen Kumar Mundlapati <Praveen Kumar Mundlapati>
2016-04-08 16:24:23 +0530

    Port to PORT

--- server.js
+++ server.js
@@ -59,5 +59,6 @@ app.put('/contactlist/:id', function (req, res) {
   );
 });
 
-app.listen(3001);
-console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
